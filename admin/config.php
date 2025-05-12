3e933fb5ab8609acf1aad9a1f31f10f48d3a2471
prologos1 <101469742+prologos1@users.noreply.github.com>
2025-05-13 01:14:51 +0300

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -5,7 +5,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 
 try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Ошибка подключения к БД: " . $e->getMessage());
