4587ddaa85b7e01a0157430186d932663e2ca40e
prologos1 <101469742+prologos1@users.noreply.github.com>
2025-05-17 23:56:58 +0300

    Update table_list.php

--- admin/templates/table_list.php
+++ admin/templates/table_list.php
@@ -1,5 +1,5 @@
 <h2>Список таблиц</h2>
-<ul>
+<ul class="custom-list">
     <?php foreach ($tables as $table): ?>
         <li><a href="table.php?table=<?= htmlspecialchars($table) ?>"><?= htmlspecialchars($table) ?></a></li>
     <?php endforeach; ?>
