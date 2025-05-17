d7342bcf13a3f099dd9b121917c5a3c2a087725b
prologos1 <101469742+prologos1@users.noreply.github.com>
2025-05-17 23:55:57 +0300

    Update header.php

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Админ-панель</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=1">
 </head>
 <body>
 <div class="container">
